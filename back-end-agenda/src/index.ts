373280cfd5a61f5df6ffb9dd682bf62e6615a88f
jatericx <jatericx@gmail.com>
2025-10-25 22:35:00 +0700

    add authentication

--- back-end-agenda/src/index.ts
+++ back-end-agenda/src/index.ts
@@ -1,13 +1,31 @@
 import express from "express"
 import router from "./routes/api"
+import bodyParser from "body-parser"
+import db from "./utils/database"
+
+async function init() {
+    try {
+        
+        const result = await db();
+
+        console.log("database status: ", result)
+
+        const app = express()
+        
+        app.use(bodyParser.json())
+        
+        const PORT = 3000
+        
+        app.use('/api', router)
+        
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`)
+        })
+
+    } catch (error) {
+        console.log(error)
+    }
+} 
 
 
-const app = express()
-
-const PORT = 3000
-
-app.use('/api', router)
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+init()
